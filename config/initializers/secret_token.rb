0d7b82a29ed48eacf0f75c41f1c9cc5f89c02f66
Cody Hill <codyhill@bdogm.com>
2011-11-11 18:35:20 -0600

    Initial commit meh no big deal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrivingLoyalty::Application.config.secret_token = '81f90cd0137eff1957434f1d0cfd8f781205b69a9057d558b84ec514de97587dd93efda6f65808f69483e98789a2daa8c5ddc564ce0e792824743f24dd0987b1'
